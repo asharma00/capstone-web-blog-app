aa7b12613daa622cc2835e559db8a5c2bf73bac1
asharma00 <68215372+asharma00@users.noreply.github.com>
2025-04-30 21:24:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+
+const port = 3000;
+const app = express();
+app.use(bodyParser.urlencoded( { extended : true } ));
+app.use(express.static('public'));
+app.use(express.static('node_modules/bootstrap/dist'));
+
+
+//get calls for home page
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact.ejs');
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.ejs');
+})
+
+
+//starting web server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
